023b4b3245f8267a40b2623e46d7cb4bad71e391
Leire Litwin <leirelitwin@MacBook-Pro-de-Leire.local>
2018-02-04 16:04:40 -0800

    assigment1
    assigment1

--- ballgame/Assets/scripts/playerController.cs
+++ ballgame/Assets/scripts/playerController.cs
@@ -34,7 +34,37 @@ public class playerController : MonoBehaviour {
 		if (other.gameObject.CompareTag ( "Pick Up"))
 		{
 			other.gameObject.SetActive (false);
-			count = count + 1;
+			count = count + 1; //red
+			SetCountText ();
+		}
+		if (other.gameObject.CompareTag ( "Yellow"))
+		{
+			other.gameObject.SetActive (false);
+			count = count + 2;
+			SetCountText ();
+		}
+		else if (other.gameObject.CompareTag ( "Green"))
+		{
+			other.gameObject.SetActive (false);
+			count = count + 3;
+			SetCountText ();
+		}
+		else if (other.gameObject.CompareTag ( "Pink"))
+		{
+			other.gameObject.SetActive (false);
+			count = count + 4;
+			SetCountText ();
+		}
+		else if (other.gameObject.CompareTag ( "Blue"))
+		{
+			other.gameObject.SetActive (false);
+			count = count + 5;
+			SetCountText ();
+		}
+		else if (other.gameObject.CompareTag ( "Purple"))
+		{
+			other.gameObject.SetActive (false);
+			count = count + 10;
 			SetCountText ();
 		}
 	}
@@ -42,7 +72,7 @@ public class playerController : MonoBehaviour {
 	void SetCountText ()
 	{
 		countText.text = "Count: " + count.ToString ();
-		if (count >= 7)
+		if (count >= 176) //1*9 + 2*12 + 3*15 + 4*12 + 5*8 + 10 = 9 + 24 + 45 + 48 + 40 + 10
 		{
 			winText.text = "You Win!";
 		}
