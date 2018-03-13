53a313b323a12bf59ebe3484ddf5d1bc9f2f75b1
Leire Litwin <leirelitwin@MacBook-Pro-de-Leire.local>
2018-03-13 15:11:09 -0700

    fixing branch problems
    fixing branch problems

---
+++ ballgame/Assets/scripts/button_changeScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button_changeScene : MonoBehaviour {
+
+    public int sceneNumber;
+	
+    public void ChangeScene()
+    {
+        Application.LoadLevel(sceneNumber);
+    }
+
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
