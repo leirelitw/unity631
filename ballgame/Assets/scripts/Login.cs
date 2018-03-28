ed57bf3c92bddbb2f1580ff686dfecee0a5f626b
Leire Litwin <leirelitwin@sfs-wifi-dhcp-10-143-208-126.wifi.sfsu.edu>
2018-03-28 14:34:48 -0700

    update random fixes
    update random fixes

--- ballgame/Assets/scripts/Login.cs
+++ ballgame/Assets/scripts/Login.cs
@@ -52,13 +52,13 @@ public class Login : MonoBehaviour {
 		GUI.SetNextControlName("username_field");
 		user_id = GUI.TextField(new Rect(30, 45, windowRect.width - 60, 30), user_id, 25);
 
-		GUILayout.Space(50);
+		GUILayout.Space(32);
 		
 		GUILayout.Label("Password");
 		GUI.SetNextControlName("password_field");
 		password = GUI.PasswordField(new Rect(30, 100, windowRect.width - 60, 30), password, "*"[0], 25);
 		
-		GUILayout.Space(100);
+		GUILayout.Space(130);
 
 		if (GUI.Button(new Rect(windowRect.width / 2 - 50, 145, 100, 30), "Log In")) {
 			//Submit();
