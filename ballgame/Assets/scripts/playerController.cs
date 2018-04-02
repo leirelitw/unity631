26c622b4e818b412dad8b27d5d06505b8ce435a6
Leire Litwin <leirelitwin@sfs-wifi-dhcp-10-143-208-126.wifi.sfsu.edu>
2018-04-02 14:36:22 -0700

    add audio to halloween scene
    add audio to halloween scene

--- ballgame/Assets/scripts/playerController.cs
+++ ballgame/Assets/scripts/playerController.cs
@@ -1,127 +1,4 @@
-﻿/*using UnityEngine;
-using UnityEngine.UI;
-using System.Collections;
-
-public class playerController : MonoBehaviour {
-
-	public float speed;
-	public Text countText;
-	public Text winText;
-	public Text velocityText;
-	private float maxSpeed = 15f;
-
-	private Rigidbody rb;
-	private int count;
-
-	void Start ()
-	{
-		rb = GetComponent<Rigidbody>();
-		count = 0;
-		SetCountText ();
-		winText.text = "";
-		velocityText.text = "Velocity: " + rb.velocity;
-
-	}
-
-	private void FixedUpdate()
-	{
-		float moveHorizontal = Input.GetAxis("Horizontal");
-		float moveVertical = Input.GetAxis("Vertical");// think helps with getting input from 
-
-		//   jump = Input.GetButton("Jump");
-		//  Jump(); // maybe move down
-
-		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-
-		rb.AddForce(movement * maxSpeed); //maybe see about doing AddRelativeForce
-		rb.velocity = new Vector3
-			(
-				Mathf.Clamp(rb.velocity.x, -maxSpeed , maxSpeed), //MAYBE due x +z velocity or something. Like if going fast on x, will be going slow accelrating to z
-				//OR MAYBE INCREASE BALL MASS SINCE ADD FORCE ACCOUNTS FOR THAT. HOWEVER THAT WOULD STILL MEAN SPEEDY BALL CAN ROTATE REASONABLLY QUICK
-				// maybe experiment so speedy balls turny is what also what bouncy has but its max speed is lower
-				// then turny ball has a low mass but low maxspeed and jump height. MAYBE CALL TURNY, QUICK ACCELRATION INSTEAD, since turning is changing velocity directy and need accelration for that 
-				// plus since be light ball, would be able to get up to its max speed faster
-
-				// SO TRY ABOVE out, also had idea of turny special ability allowing it to imeditly do 90 degree
-
-				rb.velocity.y,
-
-				Mathf.Clamp(rb.velocity.z, -maxSpeed, maxSpeed)
-			);
-		//rb.velocity = Vector3.ClampMagnitude(rb.velocity, 10f);
-		velocityText.text = "Velocity: " + rb.velocity; // see if should just have this in update
-	}
-
-	void OnTriggerEnter(Collider other) 
-	{
-		if (other.gameObject.CompareTag ( "Pick Up"))
-		{
-			other.gameObject.SetActive (false);
-			count = count + 1; //red
-			SetCountText ();
-		}
-		if (other.gameObject.CompareTag ( "Yellow"))
-		{
-			other.gameObject.SetActive (false);
-			count = count + 2;
-			SetCountText ();
-		}
-		else if (other.gameObject.CompareTag ( "Green"))
-		{
-			other.gameObject.SetActive (false);
-			count = count + 3;
-			SetCountText ();
-		}
-		else if (other.gameObject.CompareTag ( "Pink"))
-		{
-			other.gameObject.SetActive (false);
-			count = count + 4;
-			SetCountText ();
-		}
-		else if (other.gameObject.CompareTag ( "Blue"))
-		{
-			other.gameObject.SetActive (false);
-			count = count + 5;
-			SetCountText ();
-		}
-		else if (other.gameObject.CompareTag ( "Purple"))
-		{
-			other.gameObject.SetActive (false);
-			count = count + 10;
-			SetCountText ();
-		}
-		else if (other.gameObject.CompareTag ( "Black"))
-		{
-			other.gameObject.SetActive (false);
-			count = count - 10;
-			SetCountText ();
-		}
-		else if (other.gameObject.CompareTag ( "WaterTag"))
-		{
-			other.gameObject.SetActive (false);
-			count = count - 1;
-			SetCountText ();
-		}
-		else if (other.gameObject.CompareTag ( "RainbowCube"))
-		{
-			other.gameObject.SetActive (false);
-			count = count + 30;
-			SetCountText ();
-		}
-	}
-
-	void SetCountText ()
-	{
-		countText.text = "Count: " + count.ToString ();
-		if (count >= 176) //1*9 + 2*12 + 3*15 + 4*12 + 5*8 + 10 = 9 + 24 + 45 + 48 + 40 + 10
-		{
-			winText.text = "You Win!";
-		}
-	}
-}
-*/
-
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 
@@ -136,7 +13,8 @@ public class playerController : MonoBehaviour {
 	public string typeOfBall;
 	private float acceleration;
 	private float maxSpeed;
-
+	private AudioSource[] AS;
+	private int previousCount = 0;
 
 	private Rigidbody rb;
 	private int count;
@@ -256,13 +134,50 @@ public class playerController : MonoBehaviour {
 		}
 	}
 
-	void SetCountText ()
+	void SetCountText()
 	{
-		countText.text = "Count: " + count.ToString ();
+		countText.text = "Count: " + count.ToString();
+		if (count - previousCount >= 10)
+		{
+			AudioSource Pick10 = AS[6];
+			Pick10.Play();
+		}else if (count - previousCount >= 5)
+		{
+			AudioSource Pick5 = AS[5];
+			Pick5.Play();
+		}
+		else if (count - previousCount >= 5)
+		{
+			AudioSource Pick4 = AS[4];
+			Pick4.Play();
+		}
+		else if (count - previousCount >= 3)
+		{
+			AudioSource Pick3 = AS[3];
+			Pick3.Play();
+		}
+		else if (count - previousCount >= 2)
+		{
+			AudioSource Pick2 = AS[2];
+			Pick2.Play();
+		}
+		else if (count - previousCount >= 1)
+		{
+			AudioSource Pick1 = AS[1];
+			Pick1.Play();
+		}
+		else if (count - previousCount <= -1)
+		{
+			AudioSource PickBad = AS[0];
+			PickBad.Play();
+		}
 		if (count >= 176) //1*9 + 2*12 + 3*15 + 4*12 + 5*8 + 10 = 9 + 24 + 45 + 48 + 40 + 10
 		{
+			AudioSource PickWin = AS[7];
+			PickWin.Play();
 			winText.text = "You Win!";
 		}
+		previousCount = count;
 	}
 
 	void changeCursorVisibility()
