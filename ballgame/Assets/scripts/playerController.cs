546bec0f3a4bb3145cc11556de93e8af3594b38d
Leire Litwin <leirelitwin@sfs-wifi-dhcp-10-143-208-126.wifi.sfsu.edu>
2018-04-02 14:19:58 -0700

    update ball types
    update ball types

--- ballgame/Assets/scripts/playerController.cs
+++ ballgame/Assets/scripts/playerController.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+﻿/*using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 
@@ -117,5 +117,156 @@ public class playerController : MonoBehaviour {
 		{
 			winText.text = "You Win!";
 		}
+	}
+}
+*/
+
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+
+
+	//NOTE TO CHANGE FRICTION SHOULD MESS WITH ANGULAR DRAG IN RIGIDBODY. TEST IT OUT
+	public Text countText;
+	public Text winText;
+	public Text velocityText;
+	public Text accelerationText;
+	public string typeOfBall;
+	private float acceleration;
+	private float maxSpeed;
+
+
+	private Rigidbody rb;
+	private int count;
+
+	void Start ()
+	{
+		//Cursor.visible = false; allows removing cursor, though not implemented yet
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+		velocityText.text = "Velocity: " + rb.velocity;
+
+		switch (typeOfBall) {
+		case "Speedy":
+			acceleration = 12;
+			maxSpeed = 25;
+			break;
+		case "Jumpy":
+			acceleration = 10;
+			maxSpeed = 15;
+			break;
+		case "Average":
+			acceleration = 12;
+			maxSpeed = 18;
+			break;
+		case "Turny":
+			acceleration = 20;
+			maxSpeed = 14;
+			break;
+		}
+
+	}
+
+	private void FixedUpdate()
+	{
+		if(Input.GetKeyDown(KeyCode.P))
+		{
+			changeCursorVisibility();
+		}
+
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical"); 
+
+
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce(movement * acceleration, ForceMode.Acceleration);//adds force so ball can move      
+		accelerationText.text = "Accelration: " + movement * acceleration; // remove in actual game, used for testing
+
+		rb.velocity = new Vector3
+			(
+				Mathf.Clamp(rb.velocity.x, -maxSpeed , maxSpeed), 
+
+				rb.velocity.y,
+
+				Mathf.Clamp(rb.velocity.z, -maxSpeed, maxSpeed)
+			);
+		velocityText.text = "Velocity: " + rb.velocity;
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ( "Pick Up"))
+		{
+			other.gameObject.SetActive (false);
+			count = count + 1; //red
+			SetCountText ();
+		}
+		if (other.gameObject.CompareTag ( "Yellow"))
+		{
+			other.gameObject.SetActive (false);
+			count = count + 2;
+			SetCountText ();
+		}
+		else if (other.gameObject.CompareTag ( "Green"))
+		{
+			other.gameObject.SetActive (false);
+			count = count + 3;
+			SetCountText ();
+		}
+		else if (other.gameObject.CompareTag ( "Pink"))
+		{
+			other.gameObject.SetActive (false);
+			count = count + 4;
+			SetCountText ();
+		}
+		else if (other.gameObject.CompareTag ( "Blue"))
+		{
+			other.gameObject.SetActive (false);
+			count = count + 5;
+			SetCountText ();
+		}
+		else if (other.gameObject.CompareTag ( "Purple"))
+		{
+			other.gameObject.SetActive (false);
+			count = count + 10;
+			SetCountText ();
+		}
+		else if (other.gameObject.CompareTag ( "Black"))
+		{
+			other.gameObject.SetActive (false);
+			count = count - 10;
+			SetCountText ();
+		}
+		else if (other.gameObject.CompareTag ( "WaterTag"))
+		{
+			other.gameObject.SetActive (false);
+			count = count - 1;
+			SetCountText ();
+		}
+		else if (other.gameObject.CompareTag ( "RainbowCube"))
+		{
+			other.gameObject.SetActive (false);
+			count = count + 30;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText ()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 176) //1*9 + 2*12 + 3*15 + 4*12 + 5*8 + 10 = 9 + 24 + 45 + 48 + 40 + 10
+		{
+			winText.text = "You Win!";
+		}
+	}
+
+	void changeCursorVisibility()
+	{
+		Cursor.visible = !(Cursor.visible);
 	}
 }
