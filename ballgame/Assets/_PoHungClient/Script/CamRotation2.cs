8e57d645fa1e50e365b04a74e700b085d7c34522
PoHung Wang <wang.po.hung@gmail.com>
2018-05-01 21:48:20 -0700

    Create a new helloween scene for single player and testing
    -create single player script for movement
    -modify camera rotation script so the player not in the center of the view
    -create combo text for single player
    -modify the single player controller for speeding up the movement.

---
+++ ballgame/Assets/_PoHungClient/Script/CamRotation2.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamRotation2 : MonoBehaviour {
+
+	public float turnSpeed = 2.0f;
+	public Transform player;
+    public float cameraCenterOffset= 5f;
+
+	private Vector3 offset;
+    private Vector3 viewCenterOffset;
+
+	void Start () {
+		//offset = new Vector3(player.position.x, player.position.y, player.position.z);
+		offset = transform.position - player.transform.position;
+        viewCenterOffset = new Vector3(0, cameraCenterOffset, 0);
+
+    }
+
+	void LateUpdate()
+	{
+		offset = Quaternion.AngleAxis (Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
+		transform.position = player.position + offset;
+
+        //transform.LookAt(player.position); this will keep the play in the center of the view
+        
+        transform.LookAt(player.position + viewCenterOffset);
+
+    }
+}
